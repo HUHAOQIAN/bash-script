88ec53cedccf4f0f549974af42295f67baf5d105
jazzshow <jazzshow2017@gmail.com>
2023-02-04 14:55:41 +0800

    update

--- iron-fish/mine.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pid=$(ps -ef | grep "node /usr/bin/ironfish miners:start" | grep -v grep | awk '{print $2}')
-  if [ -z "$pid" ]; then
-    echo "挖矿进程没有运行，现在开始."
-    nohup ironfish miners:start &
-  else
-    echo "挖矿进程已经在运行."
-  fi
